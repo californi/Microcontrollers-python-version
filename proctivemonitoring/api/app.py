8e42fa5c3cf50edf4636b6c5b8431b1b79087731
californi <bentor.siqueira@gmail.com>
2020-09-13 16:15:04 -0300

    removing rabbitmq
    removing rabbitmq

--- proctivemonitoring/api/app.py
+++ proctivemonitoring/api/app.py
@@ -2,11 +2,15 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
 import pika
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-class SendingData(Resource):
+
+#https://www.nylas.com/blog/use-python-requests-module-rest-apis/#:~:text=How%20to%20Use%20Python%20Requests%20with%20REST%20APIs,-Now%2C%20let's%20take&text=The%20GET%20method%20is%20used,function%20to%20do%20exactly%20this.&text=The%20response%20object%20contains%20all,headers%20and%20the%20data%20payload.
+
+class ARequestingToB(Resource):
     def post(self):
         #If I am here, then the resouce Add was requested using the method POST
 
@@ -15,35 +19,28 @@ class SendingData(Resource):
 
         #If i am here, then status_code == 200
         id = str(postedData["id"])
-        description = str(postedData["description"])
-        messageBody = 'Id: ' + id + ' - Description: ' + description
+        message = str(postedData["message"])
 
-        parameters = pika.URLParameters('amqp://guest:guest@172.17.0.3:5672/%2f')
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        channel.queue_declare(queue='monitoring-queue')
-        channel.basic_publish('',
-                            'monitoring-queue',
-                            messageBody,
-                            pika.BasicProperties(content_type='text/plain',
-                                                    delivery_mode = 1))
-        connection.close()   
-
-
-        #Step 2: Add the posted data
-
-        retMap = {
-            'Message': messageBody,
-            'Status Code': 200
+        # Requesting self-diagnosis
+        query = {
+            'id': id, 
+            'message':message + ' - Message from ProactiveMonitoring'
         }
-        return jsonify(retMap)
+
+        print('executing ARequestingToB')
+
+        response = requests.get('http://microcontrollers.com/selfdiagnosis/breplyingtoa/', params=query)
+
+        print('executing ARequestingToB')
+
+        return response.json()
 
 
-api.add_resource(SendingData, "/proactive-monitoring/sendingdata")
+api.add_resource(ARequestingToB, "/proactive-monitoring/arequestingtob")
 
 @app.route('/')
 def hello_world():
-    return "Hello World! - amqp://guest:guest@172.17.0.3:5672"
+    return "Hello World! -- from proactive"
 
 
 if __name__=="__main__":
--- selfdiagnosis/api/app.py
+++ selfdiagnosis/api/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
-import pika
-
 app = Flask(__name__)
 api = Api(app)
 
-class SendingData(Resource):
+class BReplyingToA(Resource):
     def post(self):
         #If I am here, then the resouce Add was requested using the method POST
 
@@ -15,36 +13,24 @@ class SendingData(Resource):
 
         #If i am here, then status_code == 200
         id = str(postedData["id"])
-        description = str(postedData["description"])
-        messageBody = 'Id: ' + id + ' - Description: ' + description
+        message = str(postedData["message"])
 
-        parameters = pika.URLParameters('amqp://guest:guest@172.17.0.3:5672/%2f')
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        channel.queue_declare(queue='selfdiagnosis-queue')
-        channel.basic_publish('',
-                            'selfdiagnosis-queue',
-                            messageBody,
-                            pika.BasicProperties(content_type='text/plain',
-                                                    delivery_mode = 1))
-        connection.close()   
-
-
-        #Step 2: Add the posted data
-
-        retMap = {
-            'Message': messageBody,
-            'Status Code': 200
+        # Requesting self-diagnosis
+        messageReturn = {
+            'id': id, 
+            'message': message + ' - Message from Selfdiagnosis'
         }
-        return jsonify(retMap)
+        print('executing BReplyingToA')
+
+        return jsonify(messageReturn)
 
 
+api.add_resource(BReplyingToA, "/selfdiagnosis/breplyingtoa")
 
-api.add_resource(SendingData, "/selfdiagnosis/sendingdata")
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return "Hello World! -- from selfdiagnosis"
 
 
 if __name__=="__main__":
